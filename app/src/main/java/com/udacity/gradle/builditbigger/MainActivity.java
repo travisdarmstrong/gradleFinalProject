69e3783ccd399296051844c2ea3fec5273e4f893
Travis Armstrong <travisdarmstrong@gmail.com>
2018-05-12 16:20:50 -0700

    AsyncTask from MainActivity gets joke from Endpoint, then calls Android jokedisplaylibrary which displays it in an activity

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,6 +1,8 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -8,8 +10,15 @@ import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.gms.ads.MobileAds;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
+import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
+import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 import com.randomrobotics.jokedisplaylibrary.JokeDisplayActivity;
 import com.randomrobotics.jokesourcelibrary.JokeRepository;
+import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
+
+import java.io.IOException;
 
 import timber.log.Timber;
 
@@ -58,11 +67,51 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void tellJoke(View view) {
-        String joke = jokeRepository.GetJoke();
-        Intent i = new Intent(this, JokeDisplayActivity.class);
-        i.putExtra(JokeDisplayActivity.EXTRA_JOKE, joke);
-        startActivity(i);
+
+        Timber.d("starting asynctask to get joke");
+        new EndpointsAsyncTask().execute(this);
+
     }
 
 
+
+}
+
+class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
+    private static MyApi myApiService = null;
+    private Context context;
+
+    @Override
+    protected String doInBackground(Context... params) {
+        if (myApiService == null) { // only do this once
+            Timber.d("Creating api service");
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
+                    new AndroidJsonFactory(), null)
+                    //.setRootUrl("http://127.0.0.1:8080")
+                    .setRootUrl("http://10.0.2.2:8080/_ah/api")
+                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+                        @Override
+                        public void initialize(AbstractGoogleClientRequest<?> request) throws IOException {
+                            request.setDisableGZipContent(true);
+                        }
+
+                    });
+            myApiService = builder.build();
+        }
+        context = params[0];
+
+        try {
+            return myApiService.getJoke().execute().getData();
+        }catch (IOException ioEx){
+            Timber.e(ioEx);
+            return ioEx.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        Intent i = new Intent(context, JokeDisplayActivity.class);
+        i.putExtra(JokeDisplayActivity.EXTRA_JOKE, result);
+        context.startActivity(i);
+    }
 }
--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -3,6 +3,7 @@ package com.udacity.gradle.builditbigger.backend;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.randomrobotics.jokesourcelibrary.JokeRepository;
 
 import javax.inject.Named;
 
@@ -22,8 +23,19 @@ public class MyEndpoint {
     @ApiMethod(name = "sayHi")
     public MyBean sayHi(@Named("name") String name) {
         MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+        response.setData("Hello, " + name + ". I hate you");
 
+        return response;
+    }
+
+    JokeRepository jokeRepository = new JokeRepository();
+
+
+    @ApiMethod(name="getJoke")
+    public MyBean getJoke(){
+
+        MyBean response = new MyBean();
+        response.setData(jokeRepository.GetJoke());
         return response;
     }
 
